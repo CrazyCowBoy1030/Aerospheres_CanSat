8305c5338d744f39d8e40183e7580fa6e02c0a07
CrazyCowBoy1030 <64311292+CrazyCowBoy1030@users.noreply.github.com>
2025-02-14 11:11:26 +0100

    Update reciver.py

--- reciver.py
+++ reciver.py
@@ -1,17 +1,21 @@
 from SX127x.LoRa import *
 from SX127x.board_config import BOARD
 
+BOARD.setup()
+
 class MyLoRa(LoRa):
 
   def __init__(self, verbose=False):
-    BOARD.setup()
-    lora.set_freq(433.0) 
-    lora = LoRa()
-    lora.set_mode(MODE.STDBY)
     super(MyLoRa, self).__init__(verbose)
+    self.set_freq(433.0) #wkleić częstotliwość
+    self.set_mode(MODE.STDBY)
+    
     
 
-  def on_rx_done(self):
+  def listening_mode(self):
     while True:
         payload = self.read_payload(nocheck=True) 
         print(payload)
+
+LoRa_listener = MyLoRa()
+LoRa_listener.listening_mode()
